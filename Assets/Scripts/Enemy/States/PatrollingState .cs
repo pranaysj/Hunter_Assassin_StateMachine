1be02247d35d6a319470807b2469548b669a0bd1
pranaysj <pranaysj3@gmail.com>
2025-09-18 16:07:30 +0530

    Create Chasing State for Patrol Man
    Update OnePunchManStates to States & add Patrolling and Chasing states in that enum.
    Use the base class EnemyController to represent an enemy in IState instead of OnePunchManController.
    Create an interface IStateMachine and make all state machines implement this interface.
    Refactor all your state classes and state machines by updating references of IStateMachine and EnemyController.

---
+++ Assets/Scripts/Enemy/States/PatrollingState .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StatePattern.StateMachine;
+
+namespace StatePattern.Enemy
+{
+    public class PatrollingState : IState
+    {
+        public EnemyController Owner { get; set; }
+        public IStateMachine stateMachine;
+
+        public PatrollingState(IStateMachine stateMachine)
+        {
+            this.stateMachine = stateMachine;
+        }
+
+        public void OnStateEnter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnStateExit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/StateMachine/IStateMachine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern.StateMachine
+{
+    public interface IStateMachine
+    {
+        public void ChangeState(States newState);
+    }
+    public enum States
+    {
+        IDLE,
+        ROTATING,
+        SHOOTING,
+        PATROLLING,
+        CHASING
+    }
+}
